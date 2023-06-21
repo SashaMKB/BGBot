a943039ec21ad3e11c88e16f1b89c322174bca57
Тарасов Александр <tar-alex@mail.ru>
2023-06-21 13:51:57 +0300

    add some buttons and hyperlinks

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import telebot
 from telebot import types
+from aiogram.utils.markdown import hlink
 
 file = open("TOKEN.txt", "r")
 token = file.readline()
 file.close()
 bot = telebot.TeleBot(token)
+
+hello_url = hlink("Привет, я твой личный помошник", "https://youtu.be/dQw4w9WgXcQ")
 
 todos = dict()
 
@@ -19,7 +22,7 @@ HELP = '''
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Начинаем работу.\nВведите команду:")
+    bot.send_message(message.chat.id, f"{hello_url}. Начинаем работу.\nВведите команду:",parse_mode='HTML')
     bot.send_message(message.chat.id, HELP)
 
 
@@ -30,10 +33,10 @@ def help(message):
 
 @bot.message_handler(commands=['info'])
 def info(message):
-    reply_markup = types.ReplyKeyboardMarkup(resize_keyboard= True)
+    reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = "Страница новичка"
     btn2 = "Документы"
-    btn3 = "Отпуск/больничный/отгул"
+    btn3 = "Отпуск/Больничный/Отгул"
     btn4 = "Бенефит"
     btn5 = "Службы"
     btn6 = "FAQ"
@@ -59,6 +62,7 @@ def add(message):
     except Exception:
         bot.send_message(message.chat.id, "Произошла ошибка, попробуйте ещё раз")
 
+
 @bot.message_handler(commands=['show'])
 def print_(message):
     try:
@@ -75,5 +79,44 @@ def print_(message):
     except Exception:
         print(Exception)
         bot.send_message(message.chat.id, "Произошла ошибка, попробуйте снова")
+
+
+@bot.message_handler(content_types=['text'])
+def main(message):
+    if message.text == "Вернуться в главное меню":
+        reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = "Страница новичка"
+        btn2 = "Документы"
+        btn3 = "Отпуск/Больничный/Отгул"
+        btn4 = "Бенефит"
+        btn5 = "Службы"
+        btn6 = "FAQ"
+        reply_markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
+        bot.send_message(message.chat.id, "Вы вернулись в главное меню", reply_markup=reply_markup)
+    if message.text == "Документы":
+        reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = "Шаблоны"
+        btn2 = "Справки"
+        btn3 = "Расчетный лист"
+        btn4 = "Вернуться в главное меню"
+        reply_markup.add(btn1, btn2, btn3, btn4)
+        bot.send_message(message.chat.id, "Выберите нужный документ:", reply_markup=reply_markup)
+    if message.text == "Отпуск/Больничный/Отгул":
+        reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = "Отпуск"
+        btn2 = "Больничный"
+        btn3 = "Отгул"
+        btn4 = "Вернуться в главное меню"
+        reply_markup.add(btn1, btn2, btn3, btn4)
+        bot.send_message(message.chat.id, "Выберите:", reply_markup=reply_markup)
+    if message.text == "Страница новичка":
+        reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        btn1 = "Для разработчика"
+        btn2 = "Для аналиткика"
+        btn3 = "Страничка новичка БАРС Груп"
+        btn4 = "Вернуться в главное меню"
+        reply_markup.add(btn1, btn2, btn3, btn4)
+        bot.send_message(message.chat.id, "Выберите:", reply_markup=reply_markup)
+
 
 bot.polling(non_stop=True)
