6ba99b6c49baedea4437b95974f17cd8889ade7b
Тарасов Александр <tar-alex@mail.ru>
2023-06-29 11:33:10 +0300

    add crud

--- distribution.py
+++ distribution.py
@@ -2,6 +2,7 @@ import sqlite3
 import telebot
 import os
 from dotenv.main import load_dotenv
+from sqlrequests import *
 
 userWithRoots = [502643682]
 load_dotenv()
@@ -9,33 +10,16 @@ token = os.environ["TOKEN"]
 bot = telebot.TeleBot(token)
 conn = sqlite3.connect('newUsers.db', check_same_thread=False)
 cur = conn.cursor()
-# cur.execute("""CREATE TABLE IF NOT EXISTS users(
-#    userid INTEGER PRIMARY KEY autoincrement,
-#    idtelegram TEXT,
-#    startdate DATE);
-#  """)
-# conn.commit()
 
-
-def insertsql(entity):
-    cur.execute('INSERT INTO users(idtelegram,startdate) VALUES(?,?)', entity)
-
-
-def selectsql7():
-    cur.execute(
-        "SELECT idtelegram,startdate FROM users WHERE julianday('now') - julianday(startdate) >= 7 and julianday('now') - julianday(startdate) < 8")
-    return cur.fetchall()
-
-def selectsql30():
-    cur.execute(
-        "SELECT idtelegram,startdate FROM users WHERE julianday('now') - julianday(startdate) >= 30 and julianday('now') - julianday(startdate) < 31")
-    return cur.fetchall()
-
-
-def selectsql92():
-    cur.execute(
-        "SELECT idtelegram,startdate FROM users WHERE julianday('now') - julianday(startdate) >= 92 and julianday('now') - julianday(startdate) < 93")
-    return cur.fetchall()
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+   userid INTEGER PRIMARY KEY autoincrement,
+   surname TEXT,
+   name TEXT,
+   secondname TEXT,
+   idtelegram TEXT,
+   startdate DATE);
+ """)
+conn.commit()
 
 
 @bot.message_handler(commands=['add'])
@@ -45,8 +29,8 @@ def add_user(message):
 
         if user_id in userWithRoots:
             user_items = message.text.split(" ")
-            entity = (user_items[1], user_items[2])
-            insertsql(entity)
+            entity = (user_items[1], user_items[2], user_items[3], user_items[4], user_items[5])
+            insertsql(entity, cur)
             conn.commit()
         else:
             bot.send_message(message.chat.id, "У вас нет прав")
@@ -54,25 +38,42 @@ def add_user(message):
         bot.send_message(message.chat.id, "Ошибка")
 
 
-# @bot.message_handler(commands=['test'])
-# def test(message):
-#     list_of_users = selectsql()
-#     for user in list_of_users:
-#         bot.send_message(user[0], "Hello")
+@bot.message_handler(commands=["show"])
+def show(message):
+    try:
+        user_id = message.chat.id
+        if user_id in userWithRoots:
+            show_sql(bot, user_id, cur)
+        else:
+            bot.send_message(user_id, "У вас нет прав")
+    except Exception:
+        bot.send_message(message.chat.id, "Ошибка")
+
+@bot.message_handler(commands=["delete"])
+def delete(message):
+    try:
+        user_id = message.chat.id
+        if user_id in userWithRoots:
+            user_items = message.text.split(" ")
+            print(user_items[1])
+            delete_user(user_items[1], cur)
+            conn.commit()
+        else:
+            bot.send_message(user_id, "У вас нет прав")
+    except Exception:
+        bot.send_message(message.chat.id, "Ошибка")
 
 
 def check_dates():
-    list_of_users7 = selectsql7()
+    list_of_users7 = selectsql7(cur)
     for user in list_of_users7:
-        bot.send_message(user[0], "Hello my niggas")
-    list_of_users30 = selectsql30()
+        bot.send_message(user[0], "Hello my friends")
+    list_of_users30 = selectsql30(cur)
     for user in list_of_users30:
         bot.send_message(user[0], "Hello my niggas")
-    list_of_users92 = selectsql92()
+    list_of_users92 = selectsql92(cur)
     for user in list_of_users92:
         bot.send_message(user[0], "Hello my niggas")
 
-
-check_dates()
 
 bot.polling(non_stop=True)
