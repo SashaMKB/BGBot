1fe9a69f9f19dbf38558f87bc3841840b9cbeaea
Тарасов Александр <tar-alex@mail.ru>
2023-07-22 10:59:19 +0300

    change timeout

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def start(message):
     reply_markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
     bot.send_message(message.chat.id,
                      "Привет-привет!\nМеня зовут Джулиус\nРад приветствовать тебя.\nЯ помогу тебе найти "
-                     "ответы на твои вопросы.\nЧто тебя интересует?", reply_markup=reply_markup)
+                     "ответы на твои вопросы.\nЧто тебя интересует?", reply_markup=reply_markup, timeout=60)
 
 
 @bot.message_handler(commands=['instruction'])
@@ -54,7 +54,7 @@ def instruction(message):
 2) Для просмотра всех стажёров введите "/show"
 
 3) Для удаления стажера из списка стажеров введите команду "/delete (ID Telegram)"
-''')
+''',timeout=60)
 
 
 @bot.message_handler(commands=['add'])
@@ -71,9 +71,9 @@ def add_user(message):
                 insertsql2(entity, cur)
             conn.commit()
         else:
-            bot.send_message(message.chat.id, "У вас нет прав")
+            bot.send_message(message.chat.id, "У вас нет прав",timeout=60)
     except Exception:
-        bot.send_message(message.chat.id, "Ошибка")
+        bot.send_message(message.chat.id, "Ошибка",timeout=60)
 
 
 @bot.message_handler(commands=["show"])
@@ -83,9 +83,9 @@ def show(message):
         if user_id in userWithRoots:
             show_sql(bot, user_id, cur)
         else:
-            bot.send_message(user_id, "У вас нет прав")
+            bot.send_message(user_id, "У вас нет прав",timeout=60)
     except Exception:
-        bot.send_message(message.chat.id, "Ошибка")
+        bot.send_message(message.chat.id, "Ошибка",timeout=60)
 
 
 @bot.message_handler(commands=["delete"])
@@ -98,9 +98,9 @@ def delete(message):
             delete_user(user_items[1], cur)
             conn.commit()
         else:
-            bot.send_message(user_id, "У вас нет прав")
+            bot.send_message(user_id, "У вас нет прав",timeout=60)
     except Exception:
-        bot.send_message(message.chat.id, "Ошибка")
+        bot.send_message(message.chat.id, "Ошибка",timeout=60)
 
 
 # @bot.message_handler(commands=['update'])
@@ -123,15 +123,15 @@ def check_dates():
     list_of_users7 = selectsql7(cur)
     if len(list_of_users7) != 0:
         for user in list_of_users7:
-            bot.send_message(user[0], "Hello my friends")
+            bot.send_message(user[0], "Hello my friends",timeout=60)
     list_of_users30 = selectsql30(cur)
     if len(list_of_users30) != 0:
         for user in list_of_users30:
-            bot.send_message(user[0], "Hello my niggas")
+            bot.send_message(user[0], "Hello my niggas",timeout=60)
     list_of_users92 = selectsql92(cur)
     if len(list_of_users92) != 0:
         for user in list_of_users92:
-            bot.send_message(user[0], "Hello my niggas")
+            bot.send_message(user[0], "Hello my niggas",timeout=60)
 
 
 schedule.every().day.at("10:30").do(check_dates)
@@ -148,7 +148,7 @@ def main(message):
         btn5 = "Службы"
         btn6 = "Идеи"
         reply_markup.add(btn1, btn2, btn3, btn4, btn5, btn6)
-        bot.send_message(message.chat.id, text="Что Вы хотите узнать?", reply_markup=reply_markup)
+        bot.send_message(message.chat.id, text="Что Вы хотите узнать?", reply_markup=reply_markup,timeout=60)
     if message.text == "Документы":
         reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = "Шаблоны"
@@ -156,7 +156,7 @@ def main(message):
         btn4 = "Вернуться в главное меню ↩"
         reply_markup.add(btn1, btn3, btn4)
         bot.send_message(message.chat.id, "Здесь собраны самые актуальные документы, которые тебе могут понадобиться",
-                         reply_markup=reply_markup)
+                         reply_markup=reply_markup,timeout=60)
     if message.text == "Расчетный лист":
         bot.send_message(message.chat.id, '''
         "Получение расчетного листа доступно через telegram-бота БАРС Груп:
@@ -178,9 +178,9 @@ def main(message):
 1. Функционал недоступен уволившимся сотрудникам.
 2. Если бот возвращает сообщение: ""Данные по сотруднику не найдены"", обратитесь в СТП.
 3. В периоды пиковой нагрузки выгрузка листа может занять некоторое время, пока ваш запрос стоит в очереди на обработку, но он в любом случае будет выполнен."
-        ''')
+        ''',timeout=60)
     if message.text == "Шаблоны":
-        bot.send_message(message.chat.id, f"{templates_url}", parse_mode='HTML')
+        bot.send_message(message.chat.id, f"{templates_url}", parse_mode='HTML',timeout=60)
     if message.text == "Отпуск/Больничный/Отгул":
         reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = "Отпуск 🏖"
@@ -189,13 +189,13 @@ def main(message):
         btn4 = "Вернуться в главное меню ↩"
         reply_markup.add(btn1, btn2, btn3, btn4)
         bot.send_message(message.chat.id, "Оформи отпуск/больничный/отгул правильно, чтоб тебя не беспокоили в эти дни",
-                         reply_markup=reply_markup)
+                         reply_markup=reply_markup,timeout=60)
     if message.text == "Отпуск 🏖":
-        bot.send_message(message.chat.id, f"{vacation_url}", parse_mode='HTML')
+        bot.send_message(message.chat.id, f"{vacation_url}", parse_mode='HTML',timeout=60)
     if message.text == "Больничный":
-        bot.send_message(message.chat.id, f"{sickleave_url}", parse_mode='HTML')
+        bot.send_message(message.chat.id, f"{sickleave_url}", parse_mode='HTML',timeout=60)
     if message.text == "Отгул":
-        bot.send_message(message.chat.id, f"Пока что тут пусто...")
+        bot.send_message(message.chat.id, f"Пока что тут пусто...",timeout=60)
 
     if message.text == "Страница новичка🧑‍💻":
         reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -208,16 +208,16 @@ def main(message):
         bot.send_message(message.chat.id,
                          "Здесь собрана вся информация, которая будет полезна для новичка.\nЕсли останутся вопросы, "
                          "можешь обратиться к наставнику/рукводителю/HR",
-                         reply_markup=reply_markup)
+                         reply_markup=reply_markup,timeout=60)
     # нужно ли добавить страничку новичка БЦ ЖКХ --?
     if message.text == "Для разработчика":
-        bot.send_message(message.chat.id, f"{dev_url}", parse_mode="HTML")
+        bot.send_message(message.chat.id, f"{dev_url}", parse_mode="HTML",timeout=60)
     if message.text == "Для аналитика":
-        bot.send_message(message.chat.id, f"{an_url}", parse_mode='HTML')
+        bot.send_message(message.chat.id, f"{an_url}", parse_mode='HTML',timeout=60)
     if message.text == "Страничка новичка БАРС Груп":
-        bot.send_message(message.chat.id, f"{newpie_utl}", parse_mode='HTML')
+        bot.send_message(message.chat.id, f"{newpie_utl}", parse_mode='HTML',timeout=60)
     if message.text == "Страничка новичка БЦ ЖКХ,СЗ и СТРК":
-        bot.send_message(message.chat.id, f'{newjkx_url}', parse_mode="HTML")
+        bot.send_message(message.chat.id, f'{newjkx_url}', parse_mode="HTML",timeout=60)
 
     if message.text == "Плюшки":
         reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -227,15 +227,15 @@ def main(message):
         btn4 = "Библиотека"
         btn5 = "Вернуться в главное меню ↩"
         reply_markup.add(btn1, btn2, btn3, btn4, btn5)
-        bot.send_message(message.chat.id, "Выберите:", reply_markup=reply_markup)
+        bot.send_message(message.chat.id, "Выберите:", reply_markup=reply_markup,timeout=60)
     if message.text == "ДМС":
-        bot.send_message(message.chat.id, f"{dms_url}", parse_mode="HTML")
+        bot.send_message(message.chat.id, f"{dms_url}", parse_mode="HTML",timeout=60)
     if message.text == "Реферальная программа":
-        bot.send_message(message.chat.id, f"{referalprogram_url}", parse_mode="HTML")
+        bot.send_message(message.chat.id, f"{referalprogram_url}", parse_mode="HTML",timeout=60)
     if message.text == "БАРС-КОД":
-        bot.send_message(message.chat.id, f"{code_url}", parse_mode="HTML")
+        bot.send_message(message.chat.id, f"{code_url}", parse_mode="HTML",timeout=60)
     if message.text == "Библиотека":
-        bot.send_message(message.chat.id, f"{biblio_url}", parse_mode="HTML")
+        bot.send_message(message.chat.id, f"{biblio_url}", parse_mode="HTML",timeout=60)
     if message.text == "Службы":
         reply_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
         btn1 = "Бухгалтерия"
@@ -247,34 +247,34 @@ def main(message):
         btn7 = "Вернуться в главное меню ↩"
         reply_markup.add(btn1, btn2, btn3, btn4, btn5, btn6, btn7)
         bot.send_message(message.chat.id, "В БАРС Груп много служб, которые помогают организовывать работу Компании:",
-                         reply_markup=reply_markup)
+                         reply_markup=reply_markup,timeout=60)
 
     if message.text == "Бухгалтерия":
         bot.send_message(message.chat.id,
                          f"Тебе нужно заказать справку 2-НДФЛ или есть вопросы по начислению ЗП, тогда тебе в {accounting_url}",
-                         parse_mode="HTML")
+                         parse_mode="HTML",timeout=60)
     if message.text == "Техподдержка":
-        bot.send_message(message.chat.id, f"{techsupport_url}", parse_mode="HTML")
+        bot.send_message(message.chat.id, f"{techsupport_url}", parse_mode="HTML",timeout=60)
     if message.text == "Кадры":
         bot.send_message(message.chat.id,
                          f"Сколько дней отпуска? Нужен пакет документов для военкомата? Или справка с места работы? Со всеми этими вопросами поможет разобраться {personnel_url}",
-                         parse_mode="HTML")
+                         parse_mode="HTML",timeout=60)
     if message.text == "Корпоративный университет":
         bot.send_message(message.chat.id,
                          f"Корпоративный университет помогает развивать Soft и Hard навыки наших сотрудников. Список обучений, курсов, бизнес игры  можно посмотреть {corporateUn_url}",
-                         parse_mode="HTML")
+                         parse_mode="HTML",timeout=60)
     if message.text == "Пропуск 🪪":
         bot.send_message(message.chat.id,
                          f"Пропуск перестал работать или необходим пропуск на велопарковку?В этом случае необходимо поставить задачу {pass_url}",
-                         parse_mode="HTML")
+                         parse_mode="HTML",timeout=60)
     if message.text == "Канцтовары, Чай":
         bot.send_message(message.chat.id,
                          "Чай и сахар можно взять у офис-менеджера на 5 этаже.За канцелярией иди к офис-менеджеру на 7 этаж (правый вход)",
-                         parse_mode="HTML")
+                         parse_mode="HTML",timeout=60)
     if message.text == "Идеи":
         bot.send_message(message.chat.id,
                          f"Если у тебя  есть идеи, пиши {ideas_url}.Мы обязательно рассмотрим твою идею, а ты получишь классный мерч БЦ",
-                         parse_mode="HTML")
+                         parse_mode="HTML",timeout=60)
 
 
 def func1():
